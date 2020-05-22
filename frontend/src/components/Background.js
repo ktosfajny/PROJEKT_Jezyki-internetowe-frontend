a02570232f0b05027d876166c675a66aa7de9790
kto <ktosfajny777@interia.pl>
2020-05-22 22:50:29 +0200

    background - first photo

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../sass/background.scss'
+
 
 const Background = () => {
     return (
         <>
-            bg
+            <div className="background__star background__star--1"></div>
+            <div className="background__star background__star--2"></div>
+            <div className="background__star background__star--3"></div>
+            <div className="background__star background__star--4"></div>
+            <div className="background__star background__star--5"></div>
+            <div className="background__star background__star--6"></div>
+            <div className="background__star background__star--7"></div>
+            <div className="background__star background__star--8"></div>
         </>
     );
 }
