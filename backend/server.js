8e70817eeeb7e23b0bf0793e6a9c21bb05999162
kto <ktosfajny777@interia.pl>
2020-05-22 22:22:06 +0200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`server is listening on port: ${port}`)
+})
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({
+        text: 'hallo'
+    })
+})
