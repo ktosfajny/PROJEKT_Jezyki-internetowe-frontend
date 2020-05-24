d77d976cf479689e667782d87d7cfeb3c009591b
kto <ktosfajny777@interia.pl>
2020-05-24 15:30:05 +0200

    change recruit section look and add first styles for the left (questions) side

--- frontend/src/components/Recruit.js
+++ frontend/src/components/Recruit.js
@@ -36,7 +36,19 @@ class Recruit extends React.Component {
                         <span className="recruit__btn-span"></span>
                     </div>
 
-                    <div className="recruit__questionsBox"></div>
+                    <div className="recruit__questionsBox">
+                        <div className="recruit__cybershape">
+                            <div className="recruit__ls">
+                                <div className="recruit__questionHolder">
+                                    <h3 className="recruit__questionNR">1. what would you do when see an alien?</h3>
+                                    <p className="recruit__questionInfo">Every action takes reaction so pay atention to what you're doing. Depending on what would you do, you may obtain an ally, an foe, friend or sometimes even someone who can change the way you see the world.</p>
+                                </div>
+                            </div>
+                            <div className="recruit__rs">
+
+                            </div>
+                        </div>
+                    </div>
 
 
                 </div>
