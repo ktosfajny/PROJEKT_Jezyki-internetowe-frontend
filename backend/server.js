1ca519b7dbc0fadd4cc526353d99d210ab17f9d9
kto <ktosfajny777@interia.pl>
2020-05-24 17:39:57 +0200

    first attepmt to response from server but without fetch on one frontend side yet

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,54 @@
 const express = require('express')
-
 const app = express()
-
 const port = 5000;
+
+app.use(express.json())
+
+// const questions = require('./routes/questions')
 
 app.listen(port, () => {
     console.log(`server is listening on port: ${port}`)
 })
 
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
-    res.json({
-        text: 'hallo'
-    })
+
+    res.send('this is server. There is nothing to do here!')
+
 })
+
+
+
+
+const questionsDB = [
+    { questionNR: `1. what would you do when see an alien?`, questionInfo: `Every action takes reaction so pay atention to what you're doing. Depending on what would you do, you may obtain an ally, an foe, friend or sometimes even someone who can change the way you see the world.`, answerInfo: [`I would try to hide and plug my computer to the local web so I can observe the other beeing and decide what should I do.`, `New creature? It means only one thing: New preparation for the laboratory. I would try to get some of its DNA or blood and examine it.`, ` I'm not interested in living beeings. Only creatures that surrended their live to the machines are worth checking! I would let it go.`, `There is only one way to survive. You must be strong. I would let it go, but if it would be dangerous I would be less polite...`] },
+    { questionNR: `2. asdasdasdasdas?`, questionInfo: `dfbdffsdbdfbdf`, answerInfo: [`dfgdfgdfgfdg`, `tjsnr`, ` yjyrsrthb`, `segrsg`] },
+    { questionNR: `3. gthtrh`, questionInfo: `rewgew`, answerInfo: [`dh`, `htht`, ` yukmdudjy`, `myfg`] },
+    { questionNR: `4. trhjy6h?`, questionInfo: `43657t5rhf`, answerInfo: [`th5y7`, `7irs6h`, `6u6s6h`, `5weg`] },
+]
+
+
+app.post('/api/question', (req, res) => {
+
+    const { nextQuestionNR, stats } = req.body
+
+
+    //jeśli odpowiedziales juz na wszystko to tutaj zwroci się ostateczny wynik
+    if (nextQuestionNR === questionsDB.length) {
+        res.json({
+            result: 'result' //tutaj użytję 'stats'
+        })
+
+    } else {
+
+        const nextQuestion = questionsDB[nextQuestionNR]
+        res.json({
+            question: nextQuestion,
+        })
+
+    }
+
+})
+
+// questions(app)
