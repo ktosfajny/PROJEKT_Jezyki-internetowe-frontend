9b7eb98d0a1a38a404a94cb551d6ec78adbffc94
kto <ktosfajny777@interia.pl>
2020-05-25 01:44:50 +0200

    add ability to add classes by calling infobox component

--- frontend/src/components/Infobox.js
+++ frontend/src/components/Infobox.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Infobox = (props) => {
 
     return (
-        <div className="aboutme__info-box">
-            <h3 className="aboutme__info-title">{props.title}</h3>
-            <p className="aboutme__info">{props.message}<span className="aboutme__cursor">|</span>
+        <div className={props.wrapperClass}>
+            <h3 className={props.titleClass}>{props.title}</h3>
+            <p className={props.infoClass}>{props.message}<span className={props.cursorClass}>|</span>
             </p>
         </div>
     )
