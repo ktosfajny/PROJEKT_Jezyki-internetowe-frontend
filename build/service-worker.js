4f286581e63a11362132001f9f9b607c92ffa0dc
kto <ktosfajny777@interia.pl>
2020-06-03 00:44:34 +0200

    add build dir

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.443f1ea15ba91ba41c8759188ea6a7fa.js"
+  "/PROJEKT_Jezyki-internetowe-frontend/precache-manifest.d0c0962039ba88777f25b5bbb5ad7584.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PROJEKT_Jezyki-internetowe-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
