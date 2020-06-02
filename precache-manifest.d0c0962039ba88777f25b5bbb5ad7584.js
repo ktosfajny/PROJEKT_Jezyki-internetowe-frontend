c20b5647d3a181fc3893f0c39956c1c5e4c786d1
kto <ktosfajny777@interia.pl>
2020-06-03 00:58:12 +0200

    Updates

---
+++ precache-manifest.d0c0962039ba88777f25b5bbb5ad7584.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fa9899281981486202c7ba0d738927",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/index.html"
+  },
+  {
+    "revision": "f9576417f30e6339fbe6",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/css/main.7a008922.chunk.css"
+  },
+  {
+    "revision": "3ead6e00494c1e6c226a",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/js/2.4e1d9d35.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/js/2.4e1d9d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9576417f30e6339fbe6",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/js/main.f4012f2e.chunk.js"
+  },
+  {
+    "revision": "501c36ceadea4f9be4dc",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/js/runtime-main.55725ddb.js"
+  },
+  {
+    "revision": "107497cace4f144f3e03a1fbc6bdedb6",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/hexagon-bg.107497ca.jpg"
+  },
+  {
+    "revision": "13901a4c84cdd03f1d675b4c16dfb512",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/netrunner.13901a4c.png"
+  },
+  {
+    "revision": "625e6cad5a695d43e1a29f9cb9fb776a",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star1.625e6cad.png"
+  },
+  {
+    "revision": "da5717ace27d22d9cca91c740a5e9dee",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star2.da5717ac.png"
+  },
+  {
+    "revision": "3b700035ec6a8b66a84a1dfa009d00e6",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star3.3b700035.png"
+  },
+  {
+    "revision": "f8356fb9cf6969ec13c62b0d2f1e9c88",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star4.f8356fb9.png"
+  },
+  {
+    "revision": "802c13ccdcbdff21ae57f9db5c32d185",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star5.802c13cc.png"
+  },
+  {
+    "revision": "947906d8cf28b84976c329b5275c21a3",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star6.947906d8.png"
+  },
+  {
+    "revision": "77eac06ed97d1aaf9f63d880d6783aed",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star7.77eac06e.png"
+  },
+  {
+    "revision": "fdbea80cdd19aa3ea8740856452012bd",
+    "url": "/PROJEKT_Jezyki-internetowe-frontend/static/media/star8.fdbea80c.png"
+  }
+]);
